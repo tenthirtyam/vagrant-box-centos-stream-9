561f2f498712cb6cf243c90d648562b610ca55ec
Brett Taylor <brett.w.taylor@gmail.com>
2024-06-25 13:06:17 -0400

    add updates (#4)

--- provision.sh
+++ provision.sh
@@ -7,10 +7,31 @@ echo ":::: Home dir: $HOME ..."
 
 ## Disable selinux
 sudo setenforce 0
-sudo sed -i 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 
 ## Upgrade all packages
 sudo yum upgrade -y
-sudo yum install -y cloud-utils-growpart
 sudo yum groupinstall -y "Server with GUI"
 sudo systemctl set-default graphical
+sudo yum install -y epel-release
+sudo yum install -y \
+    cloud-utils-growpart \
+    cloud-init \
+    vim \
+    wget \
+    htop \
+    gcc \
+    make \
+    net-tools \
+    python3 \
+    python3-devel \
+    git \
+    zlib \
+    bzip2-devel \
+    ncurses-devel \
+    libffi-devel \
+    readline-devel \
+    openssl-devel \
+    sqlite-devel \
+    xz-devel \
+    zlib-devel
